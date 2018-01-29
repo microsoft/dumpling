3e3c04078df6720b2bd73034943194608b18fb80
Anipik <anirudhagnihotry098@gmail.com>
2018-01-29 13:17:32 -0800

    creating folder path if path doesnot exists

--- src/web/Content/client/dumpling.py
+++ src/web/Content/client/dumpling.py
@@ -71,6 +71,9 @@ class Output:
         
             # sometimes amend our essential output to an existing log file.
             if Output.s_logPath is not None:
+                parent_directory = os.path.dirname(Output.s_logPath)
+                if not os.path.exists(parent_directory):
+                    os.makedirs(parent_directory)
                 with open(Output.s_logPath, 'a') as log_file:
                     log_file.write(output + '\n')
         finally:
